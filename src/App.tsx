e3f16defc744352aac0bad1181619a5661d635df
Sage Wall <swall@esri.com>
2025-01-24 10:37:56 -0700

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "@arcgis/core/assets/esri/themes/light/main.css";
+import "@arcgis/map-components/components/arcgis-map";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <arcgis-map></arcgis-map>
+    </>
+  );
+}
+
+export default App;
